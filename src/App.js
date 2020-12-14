1f76785022d27456e461bc2a02046c4fb139ceb4
Dawid Owizyc <dawid_owizyc@hotmail.com>
2020-12-14 12:03:06 +0100

    V2 add table

--- src/App.js
+++ src/App.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import './App.css';
 
 function App() {
@@ -5,10 +6,22 @@ function App() {
     <div className="App">
       <header className="App-header">
 
-        <p>
-          Edit New
-        </p>
-
+      <table className="table" >  
+                <thead className="thead-dark">  
+                    <tr>  
+                        <th scope="col">Author</th>  
+                        <th scope="col">Title</th>
+                        <th scope="col">Price</th> 
+                        <th scope="col">Genre</th> 
+                        <th scope="col">Description</th> 
+                    </tr>  
+                </thead>  
+                <tbody>  
+                    
+                    
+                </tbody>  
+            </table>  
+        
       </header>
     </div>
   );
